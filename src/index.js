c618f2a19405743dc5b633a749c9a61801e60ee8
KristynaBartonova <kikisbartonova@gmail.com>
2020-11-01 12:09:27 +0100

    dalsichange

--- src/index.js
+++ src/index.js
@@ -2,3 +2,11 @@ import './index.html';
 import './style.css';
 
 console.log('funguju!');
+
+const tlacNavigace = document.querySelector('#nav-btn');
+const Navigace = document.querySelectior('nav');
+const Navlinks = document.querySelectorAll('nav a');
+
+tlacNavigace.addEventListener('click', () => {
+Navigace.classlist.toggle('nav-closed');
+});
