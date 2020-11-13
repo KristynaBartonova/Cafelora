651385725763fa77942c22549866f98f2560ffc6
KristynaBartonova <kikisbartonova@gmail.com>
2020-11-13 13:56:37 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,29 @@ import './style.css';
 
 console.log('funguju!');
 
+const menicko = document.querySelector('#nav-btn');
+let navigace = document.querySelector('#nav');
+const vyber = document.querySelectorAll('.nav a');
+
+menicko.addEventListener('click',() => {
+  navigace.classList.toggle('nav-closed');}
+  );
+
+vyber.addEventListener('click',() => {
+  navigace.classList.add('nav-closed');}
+  );
+
+  //ukol 4//
+
+  let ordered = false;
+
+  const DrinkCup = document.querySelector('#order-btn');
+  
+  DrinkCup.addEventListener('click',() => 
+  { DrinkCup.classList.toggle('--selected');
+});
+
+
 
 
 
